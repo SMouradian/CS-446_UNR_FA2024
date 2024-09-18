c33ab064de686e1c8e03131f8cd341728f7466eb
SamMouradian <113820652+SMouradian@users.noreply.github.com>
2024-09-18 06:16:57 +0000

    Push 3: Updates to simpleshell.c

--- simpleshell.c
+++ simpleshell.c
@@ -12,27 +12,38 @@
 #include <unistd.h>
 
 //FUNCTION DECLARATIONS
-int parseInput(char* input, char splitwords[] [500], int maxWords);
+int parseInput(char* input, char splitwords[][500], int maxWords);
 void changeDirectories(const char* path);
 int executeCommand(char const* enteredCommand, const char* infile, const char* outfile);
 
 
 //MAIN DRIVER
 int main(){
+    char cwd[500];
+    if(getcwd(cwd, sizeof(cwd)) != NULL){
+        printf("@smouradian ➜ %s$\n", cwd);
+    }
 
+    return 0;
 }
 
 
 //FUNCTION DEFINITIONS
-int parseInput(char* input, char splitwords[] [500], int maxWords){
+int parseInput(char* input, char splitWords[][500], int maxWords){
+    int count;
+    const char* tokens;
+
+    tokens = strtok(input, " ");
+    while(tokens != NULL && count < maxWords){
+        strcpy(splitWords[maxWords], tokens);
+        splitWords[count][499] = '\0';
+        count++;
+        tokens = strtok(NULL, " ");
+    }
+
+    return count;
 }
 
-void changeDirectories(const char* path){
-    fork();
-}
+//void changeDirectories(const char* path){}
 
-int executeCommand(char const* enteredCommand, const char* infile, const char* outfile){
-    fork();
-    dup2(infile, outfile);              //appropriately redirects stdin & stdout
-    execvp(infile, enteredCommand);     //launches new child process
-}
+//int executeCommand(char const* enteredCommand, const char* infile, const char* outfile){}

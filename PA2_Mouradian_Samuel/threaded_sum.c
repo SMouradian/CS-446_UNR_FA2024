1c279b8bfe9af7f5c8d42000d295f9f45f5631d8
SamMouradian <113820652+SMouradian@users.noreply.github.com>
2024-10-01 23:55:39 +0000

    Updated PA2

--- PA2_Mouradian_Samuel/threaded_sum.c
+++ PA2_Mouradian_Samuel/threaded_sum.c
@@ -4,3 +4,39 @@
 //Professor -> Christos Papachristos
 //Due Date -> 10.03.2024
 
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+//FUNCTION DECLARATIONS
+int parseInput(char* input, char splitwords[][500], int maxWords);
+int readFile(char[], int[]);
+void* arraySum(void*);
+
+
+//MAIN DRIVER
+int main(){
+
+}
+
+
+//FUNCTION DEFINITIONS
+int parseInput(char* input, char splitWords[][500], int maxWords){
+    int count = 0;
+    const char* delim = " \t\n";
+    char* tokens = strtok(input, delim);
+
+    while(tokens != NULL && count < maxWords){
+        strcpy(splitWords[count], tokens);
+        tokens = strtok(NULL, delim);
+        count++;
+    }
+
+    return count;
+}

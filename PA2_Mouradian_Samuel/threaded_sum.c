8bcd2a8031a5a7c68ceb45f81325717612e92d9f
SamMouradian <113820652+SMouradian@users.noreply.github.com>
2024-10-02 06:01:55 +0000

    completed parse and readFile function

--- PA2_Mouradian_Samuel/threaded_sum.c
+++ PA2_Mouradian_Samuel/threaded_sum.c
@@ -15,28 +15,70 @@
 
 
 //FUNCTION DECLARATIONS
-int parseInput(char* input, char splitwords[][500], int maxWords);
+int parseInput(char, char* [], char**, int*);
 int readFile(char[], int[]);
 void* arraySum(void*);
 
+//ADDITIONAL DECLARATIONS
+typedef struct _thread_data_t{
+    const int* data;
+    int startInd, endInt;
+    pthread_mutex_t* lock;
+    long long int* totalSum;
+} _thread_data_t;
+
 
 //MAIN DRIVER
-int main(){
+int main(int argc, char* argv[]){
+    char* filename;
+    int threadCount;
 
+    if(parseInput(argc, argv, &filename, &threadCount) == -1){
+        return -1;
+    }
+    else{
+        //call readFile function
+    }
+
+    return 0;
 }
 
 
 //FUNCTION DEFINITIONS
-int parseInput(char* input, char splitWords[][500], int maxWords){
-    int count = 0;
-    const char* delim = " \t\n";
-    char* tokens = strtok(input, delim);
+int parseInput(char args, char* argv[], char** file, int* numThreads){
+    if(args != 3){
+        printf("Error: Too few arguments\n");
+        return -1;
+    }
+    else{
+        *file = argv[1];
+        *numThreads = atoi(argv[2]);
 
-    while(tokens != NULL && count < maxWords){
-        strcpy(splitWords[count], tokens);
-        tokens = strtok(NULL, delim);
+        if(*numThreads <= 0){
+            printf("Error: No threads present\n");
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+int readFile(char filename[], int intArray[]){
+    FILE *file = fopen(filename, "r");
+    if(!file){
+        printf("Error: File failed to open");
+        return -1;
+    }
+
+    int count = 0, value;
+
+    while(fscanf(file, "%d", &value) == 1){
+        intArray[count] = value;
         count++;
     }
 
+    fclose(file);
     return count;
 }
+
+/*void* arraySum(void* thread_data_t){}*/

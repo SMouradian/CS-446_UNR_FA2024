9e762384d9f65afc52a771c8da26783cbe57e138
SamMouradian <113820652+SMouradian@users.noreply.github.com>
2024-11-18 02:49:49 +0000

    HW4 - Push 1

---
+++ makefile
@@ -0,0 +1,8 @@
+mymalloc: mymalloc.o
+	gcc -o mymalloc mymalloc.o
+
+mymalloc.o: mymalloc.c
+	gcc -c -Wall -pedantic mymalloc.c
+
+clean:
+	rm *.o mymalloc
---
+++ mymalloc.c
@@ -0,0 +1,5 @@
+// Assignment Name - Homework 4
+// Author - Samuel Mouradian
+// Computer Science 446 - Section 1001
+// Professor - Christos Papachristos
+// Due Date - 11.19.2024
